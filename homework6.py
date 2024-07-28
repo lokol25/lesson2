f37e8cd5cd972fe6ea48f8307bb512b925affa54
lokol25 <lokol.25@mail.ru>
2024-07-28 20:35:13 +0500

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -3,8 +3,8 @@ print(my_dict)
 print(my_dict.get('Андрей'))
 print(my_dict.get('Денис'))
 my_dict.update({'Сергей': 2000 , 'Олег': 1987})
-del my_dict['Никита']
-print(my_dict)
+new_my_dict = my_dict.pop('Никита')
+print(new_my_dict)
 my_set = {1,2,3,2,1}
 print(my_set)
 my_set.update({4,5})
